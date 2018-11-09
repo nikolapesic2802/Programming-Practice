2f2c050b1551269d6708d3b5473b93b4a90a8c6b
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2018-11-09 14:20:00 +0100

    1

--- Robots 2013/main.cpp
+++ Robots 2013/main.cpp
@@ -1,30 +1,130 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
+#include "robots.h"
+#define pb push_back
+using namespace std;
+pair<int,int> maxx=make_pair(INT_MAX,INT_MAX);
+int putaway(int a, int b, int t, int x[], int y[], int W[], int S[]) {
+    sort(x,x+a);
+    sort(y,y+b);
+    reverse(y,y+b);
+    vector<pair<int,int> > items(t);
+    for(int i=0;i<t;i++)
+    {
+        items[i].first=W[i];
+        items[i].second=S[i];
+    }
+    if(b==0)
+    {
+        //printf("usao!\n");
+        multiset<int> s;
+        while(items.size())
+        {
+            s.insert((items.back()).first);
+            items.pop_back();
+        }
+        multiset<int>::iterator it=s.end();
+        it--;
+        if(*it>x[a-1])
+            return -1;
+        for(int res=1;res<=t;res++)
+        {
+            for(int i=0;i<a;i++)
+            {
+                it=s.lower_bound(x[i]);
+                if(it==s.begin())
+                    continue;
+                it--;
+                s.erase(it);
+                if(s.size()==0)
+                {
+                    return res;
+                }
+            }
+        }
+    }
+    items.pb(maxx);
+    sort(items.begin(),items.end());
+    int l=1,r=t+5;
+    while(l<r)
+    {
+        int mid=(l+r)/2;
+        //printf("%i\n",mid);
+        int j=0;
+        int test=true;
+        int cnt=0;
+        priority_queue<pair<int,int> > s;
+        for(int i=0;i<a;i++)
+        {
+            //printf("Usao za %i\n",x[i]);
+            while(items[j].first<x[i])
+            {
+                //printf("Dodajem %i %i\n",items[j].first,items[j].second);
+                s.push({items[j].second,cnt++});
+                j++;
+            }
+            for(int k=0;k<mid;k++)
+            {
+                if(s.empty())
+                    break;
+                s.pop();
+                //printf("brisem!\n");
+            }
+        }
+        for(;j<t;j++)
+            s.push({items[j].second,cnt++});
+        for(int i=0;i<b;i++)
+        {
+            //printf("usao! %i\n",y[i]);
+            for(int k=0;k<mid;k++)
+            {
+                if(s.size()==0)
+                {
+                    break;
+                    break;
+                }
+                if((s.top().first)<y[i])
+                    s.pop();
+                else
+                {
+                    break;
+                    break;
+                }
+            }
+        }
+        if(s.size()!=0)
+            test=false;
+        if(test)
+        {
+            r=mid;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    if(l==t+5)
+        return -1;
+    return l;
+}
 
 #define fail(s, x...) do { \
 		fprintf(stderr, s "\n", ## x); \
 		exit(1); \
 	} while(0)
-
 #define MAX_A 50000
 #define MAX_B 50000
 #define MAX_T 500000
-
 static int X[MAX_A];
 static int Y[MAX_B];
 static int W[MAX_T];
 static int S[MAX_T];
-
-
-int putaway(int A, int B, int T, int X[], int Y[], int W[], int S[]) {
-    return 42;
-}
-
 int main() {
     int A, B, T, i;
 	int res;
 
-	FILE *f = fopen("sample1.in", "r");
+
+	//FILE *f = fopen("st5-random-uniform.in", "r");
+	FILE *f = fopen("st5-random-highW-cluster.in", "r");
 	if (!f)
 		fail("Failed to open input file.");
 
