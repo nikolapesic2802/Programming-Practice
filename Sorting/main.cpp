8dc0958d2716d29a585bb523ce32c1f143dc1740
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2019-06-13 22:12:04 +0200

    1

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -23,70 +23,87 @@ template<class T> ostream& operator<<(ostream& os, const vector<T>& a){os << '{'
 template<class T> ostream& operator<<(ostream& os, const set<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
 template<class T> ostream& operator<<(ostream& os, const multiset<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
 template<class T1,class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
-int insertionSort(int n,vector<int> a)
-{
-    a[0]=INT_MIN;
-    int sol=0;
-    for(int i=2;i<n;i++)
-    {
-        int j=i;
-        sol++;
-        while(a[j-1]>a[j])
-        {
-            swap(a[j-1],a[j]);
-            j--;
-            sol++;
-        }
-    }
-    return sol;
-}
-int quickSort(vector<int> a)
-{
-    vector<int> manje,vece;
-    if(a.size()<=1)
-        return 0;
-    int sol=0;
-    int pivot=a[0];
-    for(int i=1;i<(int)a.size();i++)
-    {
-        sol++;
-        if(a[i]<pivot)
-            manje.pb(a[i]);
-        else
-            vece.pb(a[i]);
-    }
-    sol+=quickSort(manje);
-    sol+=quickSort(vece);
-    return sol;
-}
-int mod=1234567;
-int offset=513;
 
+int mod=1234567;
+int add(int a,int b)
+{
+    a+=b;
+    if(a>=mod)
+        a-=mod;
+    return a;
+}
+int mul(int a,int b)
+{
+    return (ll)a*b%mod;
+}
+int n,x;
+const int N=32,L=N*(N+1)/2;
+int d[N][N][L];
+int dp[N][L][L];
+int cmax(int nn)
+{
+    return nn*(nn+1)/2;
+}
 int main()
 {
-	int n,x;
-	scanf("%i %i",&n,&x);
-	vector<int> perm;
-	for(int i=0;i<n;i++)
-        perm.pb(i);
-    reverse(all(perm));
-    printf("%i\n",insertionSort(n,perm));
-    return 0;
-    int cnt=0;
-    int mod=1234567;
-    do{
-        vector<int> a;
-        a.pb(0);
-        for(auto p:perm)
-            a.pb(p);
-        int xx=quickSort(perm);
-        int lim=xx*x;
-        int y=insertionSort(a.size(),a);
-        if(y<=lim)
-            cnt++;
-        if(cnt==mod)
-            cnt=0;
-    }while(next_permutation(all(perm)));
-    printf("%i\n",cnt);
+    d[0][0][0]=1;
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            for(int k=0;k<L;k++)
+            {
+                if(i-1>=0&&k-j>=0)
+                    d[i][j][k]=add(d[i][j][k],d[i-1][j][k-j]);
+                if(j-1>=0)
+                    d[i][j][k]=add(d[i][j][k],d[i][j-1][k]);
+            }
+	dp[0][0][0]=1;
+	for(int N=0;N<=31;N++)
+    {
+        for(int L1=0;L1<N;L1++)
+        {
+            int L2=N-1-L1;
+            for(int Q1=0;Q1<=cmax(L1);Q1++)
+            {
+                for(int Q2=0;Q2<=cmax(L2);Q2++)
+                {
+                    vector<int> S(cmax(L1)+cmax(L2)+1);
+                    vector<int> I2Options;
+                    for(int I2=0;I2<=cmax(L2);I2++)
+                        if(dp[L2][Q2][I2]!=0)
+                            I2Options.pb(I2);
+                    for(int I1=0;I1<=cmax(L1);I1++){
+                        if(dp[L1][Q1][I1]==0)
+                            continue;
+                        for(auto I2:I2Options)
+                            S[I1+I2]=add(S[I1+I2],mul(dp[L1][Q1][I1],dp[L2][Q2][I2]));
+                    }
+                    vector<int> IcOptions;
+                    for(int Ic=0;Ic<=L1*L2;Ic++)
+                        if(d[L1][L2][Ic]!=0)
+                            IcOptions.pb(Ic);
+                    int Q=Q1+Q2+N-1;
+                    for(int SI=0;SI<=cmax(L1)+cmax(L2);SI++){
+                        if(S[SI]==0)
+                            continue;
+                        for(auto Ic:IcOptions)
+                        {
+                            int I=SI+Ic+L1;
+                            dp[N][Q][I]=add(dp[N][Q][I],mul(S[SI],d[L1][L2][Ic]));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    vector<pair<int,int> > a={{5, 1}, {12, 2}, {18, 3}, {20, 1}, {22, 5}, {23, 3}, {24, 4}, {29, 2}, {30, 5}, {31, 1}};
+    for(int i=0;i<10;i++)
+    {
+        int n=a[i].f,x=a[i].s;
+        int sol=0;
+        for(int Q=0;Q<=cmax(n);Q++)
+            for(int I=min(cmax(n),Q*x);I>=n-1;I--)
+                sol=add(sol,dp[n][Q][I-n+1]);
+        printf("%i: %i\n",i,sol);
+    }
     return 0;
 }
