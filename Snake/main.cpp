0cc7a0fdde96f04c670dd1708961242b768d083f
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2019-06-14 18:40:55 +0200

    1

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,3 +1,6 @@
+/*
+    https://github.com/mostafa-saad/MyCompetitiveProgramming/blob/master/Olympiad/CEOI/official/2008/editorials.pdf
+*/
 #include <bits/stdc++.h>
 #include "snakelib.h"
 
@@ -13,28 +16,73 @@
 
 using namespace std;
 
-/*int get_speed()
-{
-    return 5;
-}
-void ask_snake(int U1,int U2,char *A1,char *A2)
-{
-    printf("%i %i\n",U1,U2);
-    string a,b;
-    cin >> a >> b;
-    (*A1)=a[0];
-    (*A2)=b[0];
-}
-void tell_length(int L)
-{
-    printf("%i!\n",L);
-}*/
-
 int main()
 {
+    int cnt=0;
 	char a1,a2;
-	int s=get_speed();
-	int l1=0,r1=12121,l2=0,r2=12121;
-	while()
+	int k=get_speed();
+	int m=12121;
+	int l1,r1,l2,r2;
+	int a=m/3,b=2*a;
+	ask(a,b);
+	cnt++;
+	if(a1=='s')
+    {
+        if(a2=='s')
+        {
+            l1=0;r1=a;
+            l2=b;r2=m;
+        }
+        if(a2=='f')
+        {
+            l1=0;r1=a;
+            l2=a;r2=b-1;
+        }
+    }
+    if(a1=='f')
+    {
+        l1=l2=0;
+        r1=r2=a-1;
+    }
+    if(a1=='b')
+    {
+        if(a2=='s')
+        {
+            l1=a+1;r1=b;
+            l2=b;r2=m;
+        }
+        if(a2=='b')
+        {
+            l1=l2=b+1;
+            r1=r2=m;
+        }
+        if(a2=='f')
+        {
+            l1=l2=a+1;
+            r1=r2=b-1;
+        }
+    }
+	while(r1-l1+1>k+1||r2-l2+1>k+1)
+    {
+        r1=min(r1+k,m);
+        r2=min(r2+k,m);
+        int m1=(l1+r1)>>1,m2=(l2+r2+1)>>1;
+        ask(m1,m2);
+        cnt++;
+        if(a1=='s')
+            r1=m1;
+        if(a1=='f')
+            r1=m1-1;
+        if(a1=='b')
+            l1=m1+1;
+        if(a2=='s')
+            l2=m2;
+        if(a2=='f')
+            r2=m2-1;
+        if(a2=='b')
+            l2=m2+1;
+    }
+    int m1=(l1+r1)>>1,m2=(l2+r2)>>1;
+    tell_length(m2-m1+1);
     return 0;
 }
--- Snake/snake/grader/gslib.h
+++
@@ -1,7 +0,0 @@
-void initialize();
-
-void log(char const* Message);
-void die(char const* Message);
-void print_history();
-void generate_history();
-bool verify_history();
