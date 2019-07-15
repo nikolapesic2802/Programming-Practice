b83bdfc133bf53584c795d1f5d6a718584c1ea32
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2019-07-15 15:20:56 +0200

    2

---
+++ Compatible Numbers/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define ll long long
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define D(x) cerr << #x << " is " << (x) << "\n";
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>; ///find_by_order(),order_of_key()
+template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) { os << '{' << a.f << ", " << a.s << '}'; return os; }
+template<class T> ostream& operator<<(ostream& os, const vector<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
+template<class T> ostream& operator<<(ostream& os, const set<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const multiset<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T1,class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+
+const int L=22,N=1<<L;
+int main()
+{
+	int n;
+	scanf("%i",&n);
+	vector<int> num(N,-1),nxt(N,-1),a(n);
+	for(int i=0;i<n;i++)
+        scanf("%i",&a[i]),num[a[i]]=a[i];
+    for(int i=0;i<22;i++)
+    {
+        for(int x=0;x<N;x++)
+            if(x&(1<<i))
+                nxt[x]=num[x^(1<<i)];
+            else
+                nxt[x]=max(num[x],num[x^(1<<i)]);
+        num=nxt;
+    }
+    for(int i=0;i<n;i++)
+        printf("%i ",num[a[i]]);
+    return 0;
+}
