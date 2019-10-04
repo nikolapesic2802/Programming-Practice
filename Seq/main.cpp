10112eaeca06fe64ab8480b6835c2e901951758c
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2019-10-04 18:13:37 +0200

    Update main.cpp

--- Seq/main.cpp
+++ Seq/main.cpp
@@ -1,3 +1,6 @@
+/*
+    -https://github.com/mostafa-saad/MyCompetitiveProgramming/blob/master/Olympiad/Baltic/official/boi2014_solutions/sequence/sequence.tex
+*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
