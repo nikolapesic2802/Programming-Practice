3eb6562994b9317da2f4850109dec25baa44f0b8
nikolapesic2802 <36281594+nikolapesic2802@users.noreply.github.com>
2020-01-03 23:50:48 +0100

    1

--- xortransform.cpp
+++ xortransform.cpp
@@ -1,7 +1,6 @@
 /*
     -Figure out the pattern with brute force.
-    -For each field in the matrix there will be a number i such that that number will be in the final xor for every k such that k&i==i.
-    -That number can be found using the recursive formula in the calc function.
+    -For each field in the matrix (a,b) is a number i = a|b such that that number will be in the final xor for every k such that k&i==i.
     -Now we can use DP SOS to find for each value of k that is less than 2^22, what the value the field [0][0] will be after k steps.
     -If we get a value of k greater than 2^22, just remove the most significant bit until it isn't greater than 2^22 or just take k=k&(2^22-1).
 */
@@ -35,16 +34,7 @@ template<class T> ostream& operator<<(ostream& os, const multiset<T,greater<T> >
 template<class T1,class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
 
 const int L=22,N=1<<L;
-int x[N],cnt[N];
-int calc(int i,int j){
-    if(cnt[i]==-1&&cnt[j]==-1)
-        return 0;
-    if(cnt[i]==cnt[j])
-        return calc(i-(1<<cnt[i]),j-(1<<cnt[j]))+(1<<cnt[i]);
-    if(cnt[i]>cnt[j])
-        return calc(i-(1<<cnt[i]),j)+(1<<cnt[i]);
-    return calc(i,j-(1<<cnt[j]))+(1<<cnt[j]);
-}
+int x[N];
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -52,17 +42,11 @@ int main()
     cout.tie(NULL);
 	freopen("xortransform.in","r",stdin);
 	freopen("xortransform.out","w",stdout);
-	int ans=-1,tr=1;
-	for(int i=0;i<N;i++){
-        if(i==tr)
-            ans++,tr*=2;
-        cnt[i]=ans;
-    }
 	int n,m,q,a;
 	cin >> n >> m >> q;
 	for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
-            cin >> a,x[calc(i,j)]^=a;
+            cin >> a,x[i|j]^=a;
 	for(int i=0;i<L;i++)
         for(int j=0;j<N;j++)
             if(j&(1<<i))
